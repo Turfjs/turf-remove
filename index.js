5090e7764911a78d33b4d9ebdd62c8bf96f2ffe8
Morgan Herlocker <morgan.herlocker@gmail.com>
2014-04-03 10:13:14 -0400

    remove callback param

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var featureCollection = require('turf-featurecollection')
 
-module.exports = function(collection, key, val, done) {
+module.exports = function(collection, key, val) {
   var newFC = featureCollection([]);
   for(var i = 0; i < collection.features.length; i++) {
     if(collection.features[i].properties[key] != val) {
