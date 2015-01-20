734f6c26a34d41bdb2881c8b7b18ffaf47c7c9ef
Lyzi Diamond <lyzidiamond@gmail.com>
2015-01-20 15:51:46 -0800

    Move examples from Null Island to Accra, Ghana and change key-value to marker-colors

--- index.js
+++ index.js
@@ -11,16 +11,20 @@ var featureCollection = require('turf-featurecollection');
  * @param {String} value the value to filter
  * @return {FeatureCollection} the resulting FeatureCollection without features that match the property-value pair
  * @example
- * var trees = turf.featurecollection([
- *  turf.point(1,2, {species: 'oak'}),
- *  turf.point(2,1, {species: 'birch'}),
- *  turf.point(3,1, {species: 'oak'}),
- *  turf.point(2,2, {species: 'redwood'}),
- *  turf.point(2,3, {species: 'maple'}),
- *  turf.point(4,2, {species: 'oak'})
+ * var points = turf.featurecollection([
+ *  turf.point(-0.235004, 5.551918, {'marker-color': '#00f'}),
+ *  turf.point(-0.209598, 5.56439, {'marker-color': '#f00'}),
+ *  turf.point(-0.197753, 5.556018, {'marker-color': '#00f'}),
+ *  turf.point(-0.217323, 5.549526, {'marker-color': '#000'}),
+ *  turf.point(-0.211315, 5.543887, {'marker-color': '#0f0'}),
+ *  turf.point(-0.202217, 5.547134, {'marker-color': '#00f'}),
+ *  turf.point(-0.231227, 5.56644, {'marker-color': '#0f0'})
  * ]);
- * //=trees
- * var filtered = turf.remove(trees, 'species', 'oak');
+ *
+ * //=points
+ *
+ * var filtered = turf.remove(points, 'marker-color', '#00f');
+ *
  * //=filtered
 */
 module.exports = function(collection, key, val) {
