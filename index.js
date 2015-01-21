21cc97b70a168a57bf0c753ab8430a522098e5a7
Tom MacWright <tom@macwright.org>
2015-01-21 12:11:46 -0800

    Fix point form

--- index.js
+++ index.js
@@ -12,13 +12,13 @@ var featureCollection = require('turf-featurecollection');
  * @return {FeatureCollection} the resulting FeatureCollection without features that match the property-value pair
  * @example
  * var points = turf.featurecollection([
- *  turf.point(-0.235004, 5.551918, {'marker-color': '#00f'}),
- *  turf.point(-0.209598, 5.56439, {'marker-color': '#f00'}),
- *  turf.point(-0.197753, 5.556018, {'marker-color': '#00f'}),
- *  turf.point(-0.217323, 5.549526, {'marker-color': '#000'}),
- *  turf.point(-0.211315, 5.543887, {'marker-color': '#0f0'}),
- *  turf.point(-0.202217, 5.547134, {'marker-color': '#00f'}),
- *  turf.point(-0.231227, 5.56644, {'marker-color': '#0f0'})
+ *  turf.point([-0.235004, 5.551918], {'marker-color': '#00f'}),
+ *  turf.point([-0.209598, 5.56439], {'marker-color': '#f00'}),
+ *  turf.point([-0.197753, 5.556018], {'marker-color': '#00f'}),
+ *  turf.point([-0.217323, 5.549526], {'marker-color': '#000'}),
+ *  turf.point([-0.211315, 5.543887], {'marker-color': '#0f0'}),
+ *  turf.point([-0.202217, 5.547134], {'marker-color': '#00f'}),
+ *  turf.point([-0.231227, 5.56644], {'marker-color': '#0f0'})
  * ]);
  *
  * //=points
@@ -35,4 +35,4 @@ module.exports = function(collection, key, val) {
     }
   }
   return newFC;
-}
+};
