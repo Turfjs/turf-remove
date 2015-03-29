5b9c978785b254159174ae058ccc06730b43b06b
Tim Channell <tim.channell@gmail.com>
2015-03-29 18:33:11 -0400

    eslint fixes

--- index.js
+++ index.js
@@ -91,7 +91,7 @@ var featureCollection = require('turf-featurecollection');
 module.exports = function(collection, key, val) {
   var newFC = featureCollection([]);
   for(var i = 0; i < collection.features.length; i++) {
-    if(collection.features[i].properties[key] != val) {
+    if(collection.features[i].properties[key] !== val) {
       newFC.features.push(collection.features[i]);
     }
   }
